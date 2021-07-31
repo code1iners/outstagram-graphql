2eedba586ded00944e49f1eface1ad0ed67d7b45
Codeliner <codeliner96@gmail.com>
2021-07-31 18:46:35 +0900

    Implement see profile by username.

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import client from "../client";
 
 export default {
@@ -6,15 +7,33 @@ export default {
       _,
       { firstName, lastName, username, email, password }
     ) => {
-      // cnote. check if username or email are already on DB..
-      const foundUser = await client.user.findFirst({
-        where: {
-          OR: [{ username }, { email }],
-        },
-      });
-      // note. hash password.
+      try {
+        // cnote. check if username or email are already on DB..
+        const alreadyUserExistsYouKnowWhatIsThis = await client.user.findFirst({
+          where: {
+            OR: [{ username }, { email }],
+          },
+        });
+        if (alreadyUserExistsYouKnowWhatIsThis) {
+          throw new Error("This username/email is already exists.");
+        }
 
-      // note. save and return the user.
+        // note. hash password.
+        const fuckingPassword = await bcrypt.hash(password, 10);
+
+        // note. save and return the user.
+        return client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: fuckingPassword,
+          },
+        });
+      } catch (sexyError) {
+        return sexyError;
+      }
     },
   },
 };
