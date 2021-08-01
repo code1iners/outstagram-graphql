44f53d108a08874dcbf4da92782fa77d87f51499
Codeliner <codeliner96@gmail.com>
2021-08-01 11:40:07 +0900

    Implement update user pforile 1.

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,7 +1,38 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
 export default {
   Mutation: {
-    editProfile: () => {
-      console.log("hello");
+    editProfile: async (
+      _,
+      { firstName, lastName, username, email, password: newPassword }
+    ) => {
+      let hashedPassword = null;
+      if (newPassword) {
+        hashedPassword = await bcrypt.hash(newPassword, 10);
+      }
+      const updatedUser = await client.user.update({
+        where: {
+          id: 3,
+        },
+        data: {
+          firstName,
+          lastName,
+          username,
+          email,
+          ...(hashedPassword && { password: hashedPassword }),
+        },
+      });
+      if (updatedUser.id) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "Could not update profile.",
+        };
+      }
     },
   },
 };
