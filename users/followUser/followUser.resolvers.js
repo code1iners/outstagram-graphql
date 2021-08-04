48c0e3e5b8737aba0525e63bf08c36d754cf0204
Codeliner <codeliner96@gmail.com>
2021-08-04 20:27:25 +0900

    Added unfollow feature.

---
+++ users/followUser/followUser.resolvers.js
@@ -0,0 +1,35 @@
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+export default {
+  Mutation: {
+    followUser: protectedResolver(async (_, { username }, { loggedInUser }) => {
+      const ok = client.user.findUnique({
+        where: {
+          username,
+        },
+      });
+      if (!ok) {
+        return {
+          ok: false,
+          error: "The user does not found.",
+        };
+      }
+      await client.user.update({
+        where: {
+          id: loggedInUser.id,
+        },
+        data: {
+          following: {
+            connect: {
+              username,
+            },
+          },
+        },
+      });
+      return {
+        ok: true,
+      };
+    }),
+  },
+};
---
+++ users/unfollowUser/unfollowUser.resolvers.js
@@ -0,0 +1,37 @@
+import client from "../../client";
+import { protectedResolver } from "../users.utils";
+
+export default {
+  Mutation: {
+    unfollowUser: protectedResolver(
+      async (_, { username }, { loggedInUser }) => {
+        const exists = client.user.findUnique({
+          where: { username },
+        });
+        if (!exists) {
+          return {
+            ok: false,
+            error: "The user does not found.",
+          };
+        }
+
+        await client.user.update({
+          where: {
+            id: loggedInUser.id,
+          },
+          data: {
+            following: {
+              disconnect: {
+                username,
+              },
+            },
+          },
+        });
+
+        return {
+          ok: true,
+        };
+      }
+    ),
+  },
+};
